a6c1b402fc0ae143bc35ed5cd25e95ba29bf4ddd
Kinsey McGlasson <33907739+KinseyMcG@users.noreply.github.com>
2020-08-26 16:03:57 -0500

    CMake compiled version

---
+++ cmake-build-debug/CMakeFiles/1_McGlasson_Kinsey.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1_McGlasson_Kinsey.dir/main.cpp.o"
+  "CMakeFiles/1_McGlasson_Kinsey.dir/card.cpp.o"
+  "CMakeFiles/1_McGlasson_Kinsey.dir/hand.cpp.o"
+  "CMakeFiles/1_McGlasson_Kinsey.dir/deck.cpp.o"
+  "CMakeFiles/1_McGlasson_Kinsey.dir/player.cpp.o"
+  "CMakeFiles/1_McGlasson_Kinsey.dir/dealer.cpp.o"
+  "1_McGlasson_Kinsey.pdb"
+  "1_McGlasson_Kinsey"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_McGlasson_Kinsey.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
